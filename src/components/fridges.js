31305302d589f2a13cd590d05a393c78c28371a9
PohSA <pohyamone18@gmail.com>
2024-03-08 15:40:03 +1100

    Updage fridge

--- src/components/fridges.js
+++ src/components/fridges.js
@@ -1,7 +1,19 @@
 import "./styles/content.css";
 function Fridges(){
     return (
-        <div className="fridge"><p>Fridges</p></div>
+        <div className="fridge">
+            <div className="fridgeListWrapper">
+            <div className="fridgeListHolder">
+                <div className = "fridge1Holder"><p>Fridge1</p></div>
+                <div className = "fridge2Holder"><p>Fridge2</p></div>
+                <div className = "fridge3Holder"><p>Fridge3</p></div>
+                <div className = "fridge4Holder"><p>Fridge4</p></div>
+                <div className = "fridge5Holder"><p>Fridge5</p></div>
+           
+            </div>
+            </div>
+
+        </div>
     )
 }
 export default Fridges;
