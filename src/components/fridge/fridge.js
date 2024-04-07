18f3f0ff4331a46f1360a5a6c2d949ad73c2f4e4
Taijitu <116534934+rankopolat@users.noreply.github.com>
2024-04-07 17:35:22 +1000

    style

--- src/components/fridge/fridge.js
+++ src/components/fridge/fridge.js
@@ -7,7 +7,8 @@ import React from "react";
 const Fridge = ({ fridge, onFridgeClick }) => {
 
     return (
-    <div className="fridge2Holder" onClick={() => onFridgeClick(fridge)}>
+        <div className={`fridge2Holder ${fridge.selected ? 'selectedFridge' : ''}`}
+        onClick={() => onFridgeClick(fridge)}>
         <img src ={fridge.selected ? openFridge : closeFridge} alt ="c" />
         <p className="caption">{fridge.fridgeName}</p>
     </div>
