faeefac8abc10603445728d1f868542aea3565d2
Taijitu <116534934+rankopolat@users.noreply.github.com>
2024-04-12 20:13:43 +1000

    added more member Icons to match color scheme

--- src/Dash.js
+++ src/Dash.js
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react';
 import axios from 'axios';
 import './Dash.css';
 
-import logo from "./components/assets/FridgeTLogo.png";
+import logo from "./components/assets/fridgeLogo.png";
 import user from "./components/assets/memberIcons/memberIcon.png";
 import darkbck from "./components/assets/moon2.png";
 import lightBck from "./components/assets/light_bck.png";
