195d02799dc4f75eb1df9f5abeaa0ee8b389df38
PohSA <pohyamone18@gmail.com>
2024-04-21 15:28:18 +1000

    shoppingList framework update

--- src/components/shoppintlist/shoppinglist.js
+++ src/components/shoppintlist/shoppinglist.js
@@ -1,20 +1,38 @@
 import './shoppingList.css';
 import {useState} from 'react';
+import sausage from '../assets/sausage.png';
 
 function ShoppingList(){
 
     const [selectedListName, setSelectedListName] = useState("SHOPPING LIST _1");
 
     return (
+        
         <div className='shoppingList'>
-            <div className="shoppingListNameHolder">
-                <div className="listNameTitle">Shopping List</div>
-                <div className="listName">{selectedListName}</div>
-                <div className="createNewS_list">Create New List</div>
+            <div><img className="sausageImage" src={sausage} alt="Logo" /></div>
 
 
-            </div>
-            <div className="individual_shoppingList">individual_shoppingList-1</div>
+            <div className='shoppingListHolder'>
+                <div className="shoppingListNameHolder">
+                    <div className="listNameTitle">Shopping List</div>
+                    <div className="listName">{selectedListName}</div>
+                    <div className="createNewS_list">Create New List</div>
+
+
+                </div>
+                <div className="individual_shoppingList">
+                    <div className='individualS_listTitle'>{selectedListName}</div>
+                    <div className='s_listItem'>All the shopping list items are in here</div>
+                    <div className='s_listOptions'>
+                        <div id="edit">Edit List</div>
+                        <div id="delete">Delete List</div>
+                        <div id="import">Import/Export</div>
+                    </div>
+
+                </div>
         </div>
+        </div>
+        
+
     )
 }export default ShoppingList;
