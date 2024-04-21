a2436f12de8dac2b915f3e3bacff59a36fa15a67
PohSA <pohyamone18@gmail.com>
2024-04-21 16:23:45 +1000

    shopping list update

--- src/components/shoppintlist/shoppinglist.js
+++ src/components/shoppintlist/shoppinglist.js
@@ -1,8 +1,11 @@
 import './shoppingList.css';
 import {useState} from 'react';
 import sausage from '../assets/sausage.png';
+import React, { CSSProperties } from 'react';
 
 function ShoppingList(){
+
+    
 
     const [selectedListName, setSelectedListName] = useState("SHOPPING LIST _1");
 
@@ -22,7 +25,27 @@ function ShoppingList(){
                 </div>
                 <div className="individual_shoppingList">
                     <div className='individualS_listTitle'>{selectedListName}</div>
-                    <div className='s_listItem'>All the shopping list items are in here</div>
+                    <div className='s_listItem'>
+                    <div className='ItemTitle'>
+                        <table id="titleTable">
+                            <tr >
+                                <td id="foodName">Food Name</td>
+                                <td id="quantity">Quantity</td>
+                                <td id="type">Type</td>
+                            </tr>
+                        </table>  
+                    </div>
+                    <div className='eachItem'> 
+                    <table id="eachItemTable">
+                            <tr >
+                                <td id="foodName">Apple</td>
+                                <td id="quantity">3</td>
+                                <td id="type">Fruit</td>
+                            </tr>
+                        </table> 
+                    </div>
+
+                    </div>
                     <div className='s_listOptions'>
                         <div id="edit">Edit List</div>
                         <div id="delete">Delete List</div>
