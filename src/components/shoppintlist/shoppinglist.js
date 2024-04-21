ec5db69302ee971b6de95df1c6e1d16c7ad4ff5f
PohSA <pohyamone18@gmail.com>
2024-04-21 14:01:20 +1000

    shoppingListNameHolder updated

--- src/components/shoppintlist/shoppinglist.js
+++ src/components/shoppintlist/shoppinglist.js
@@ -1,7 +1,20 @@
 import './shoppingList.css';
+import {useState} from 'react';
 
 function ShoppingList(){
+
+    const [selectedListName, setSelectedListName] = useState("SHOPPING LIST _1");
+
     return (
-        <div>Hello</div>
+        <div className='shoppingList'>
+            <div className="shoppingListNameHolder">
+                <div className="listNameTitle">Shopping List</div>
+                <div className="listName">{selectedListName}</div>
+                <div className="createNewS_list">Create New List</div>
+
+
+            </div>
+            <div className="individual_shoppingList">individual_shoppingList-1</div>
+        </div>
     )
 }export default ShoppingList;
