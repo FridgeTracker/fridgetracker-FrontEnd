bdee179b7d0a4b0f38e37a42c38114deb0a570ae
zayyehtut <zayyehtut.zy@gmail.com>
2024-04-26 09:00:18 +1000

    Improved layout ,Optimized ingredient availability check

--- src/components/meals/MealDetails.js
+++ src/components/meals/MealDetails.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import mealService from "../../services/mealService";
 import {
   Box,
   Button,
@@ -9,102 +10,244 @@ import {
   ListItem,
   Typography,
   Grid,
+  Chip,
 } from "@mui/material";
 import ArrowBackIosNewIcon from "@mui/icons-material/ArrowBackIosNew";
-import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
+import CheckCircleOutlineIcon from "@mui/icons-material/CheckCircleOutline";
+import CancelIcon from "@mui/icons-material/Cancel";
+import { styled } from "@mui/material/styles";
 
-const MealDetails = ({ meal, onGoBack, memberName }) => {
-  // Define a reusable card container component for layout consistency
-  const CardContainer = ({ children }) => (
-    <Card sx={{ bgcolor: "background.paper", mb: 2, p: 2 }}>{children}</Card>
+const StyledCard = styled(Card)(({ theme }) => ({
+  backgroundColor: "transparent",
+  boxShadow: "none",
+  padding: theme.spacing(2),
+}));
+
+const StyledCardMedia = styled(CardMedia)(({ theme }) => ({
+  height: 400,
+  width: 400,
+  borderRadius: "50%",
+  margin: "0 auto",
+}));
+
+const MealTitle = styled(Typography)(({ theme }) => ({
+  backgroundColor: "#1b8b60",
+  color: "#fff",
+  padding: theme.spacing(1),
+  textAlign: "right",
+}));
+
+const IngredientsList = styled(List)(({ theme }) => ({
+  marginTop: theme.spacing(1),
+  display: "flex",
+  flexDirection: "column",
+  gap: theme.spacing(1),
+  alignItems: "flex-end",
+}));
+
+const AvailabilityIndicator = styled("span")(({ theme, available }) => ({
+  height: "15px",
+  width: "15px",
+  borderRadius: "50%",
+  display: "inline-block",
+  marginLeft: theme.spacing(1),
+  backgroundColor: available
+    ? theme.palette.success.main
+    : theme.palette.error.main,
+}));
+
+const IngredientItem = styled("div")({
+  display: "flex",
+  justifyContent: "flex-end",
+  alignItems: "center",
+  marginBottom: "8px",
+});
+
+const NutritionInfo = styled(Box)(({ theme }) => ({
+  display: "flex",
+  lineHeight: "2",
+  justifyContent: "space-between",
+  borderTop: "1px solid #ccc",
+  borderBottom: "1px solid #ccc",
+  "& > div": {
+    borderRight: "1px solid #ccc",
+    paddingRight: theme.spacing(2),
+    paddingLeft: theme.spacing(2),
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    "&:last-child": {
+      borderRight: "none",
+    },
+  },
+}));
+
+const NutritionLabel = styled(Typography)(({ theme }) => ({
+  fontWeight: "bold",
+}));
+
+/*Simulated function; need to replace with actual availability check
+const MealDetails = ({ meal, onGoBack }) => {
+  const hasAllIngredientsAvailable = meal.ingredients.every((ingredient) =>
+    ingredientAvailability(ingredient)
+  );
+  */
+const MealDetails = ({ meal, onGoBack }) => {
+  const [availability, setAvailability] = useState({});
+
+  useEffect(() => {
+    const checkAvailability = async () => {
+      const availabilityResults = {};
+      for (const ingredient of meal.ingredients) {
+        availabilityResults[ingredient] =
+          await mealService.ingredientAvailability([ingredient]); // Assuming ingredientAvailability takes an array
+      }
+      setAvailability(availabilityResults);
+    };
+
+    checkAvailability();
+  }, [meal.ingredients]);
+
+  // Calculate if all ingredients are available
+  const hasAllIngredientsAvailable = meal.ingredients.every(
+    (ingredient) => availability[ingredient]
   );
 
   return (
-    <Container>
+    <Container sx={{ maxWidth: "none" }}>
       <Box sx={{ my: 4, display: "flex", alignItems: "center" }}>
         <Button
           startIcon={<ArrowBackIosNewIcon />}
           onClick={onGoBack}
           sx={{ color: "text.primary", mb: 2 }}
         >
-          {memberName}
+          Back to Meals
         </Button>
       </Box>
-      <Grid container spacing={4}>
-        {/* Image and Meal Name */}
-        <Grid item xs={12} md={6}>
-          <CardContainer>
-            <CardMedia
-              component="img"
-              image={meal.Image || "defaultMealImage.jpg"}
-              alt={meal.MealName}
-              sx={{ maxHeight: 300, objectFit: "contain", mb: 2 }}
-            />
-            <Typography variant="h4" component="div" gutterBottom>
-              {meal.MealName}
-            </Typography>
-          </CardContainer>
-        </Grid>
 
-        {/* Ingredients List */}
-        <Grid item xs={12} md={6}>
-          <CardContainer>
-            <Typography gutterBottom variant="h6" component="div">
-              Ingredients
+      <Grid
+        container
+        spacing={3}
+        sx={{ flexWrap: "nowrap", alignContent: "center" }}
+      >
+        <Grid item xs={12} md={4}>
+          <StyledCard>
+            <MealTitle variant="h4" component="div" gutterBottom>
+              {meal.mealName}
+            </MealTitle>
+            <Typography
+              variant="body2"
+              color="text.secondary"
+              textAlign={"right"}
+            >
+              {meal.description}
             </Typography>
-            <List dense>
-              {meal.Ingredients.map((ingredient, index) => (
-                <ListItem key={index}>{ingredient}</ListItem>
+            <IngredientsList>
+              {meal.ingredients.map((ingredient, index) => (
+                <IngredientItem key={index}>
+                  <Typography variant="body1">{ingredient}</Typography>
+                  <AvailabilityIndicator
+                    available={availability[ingredient] || false}
+                  />
+                </IngredientItem>
               ))}
-            </List>
+            </IngredientsList>
             <Box sx={{ display: "flex", justifyContent: "flex-end", mt: 2 }}>
               <Button
                 variant="contained"
+                disabled={hasAllIngredientsAvailable}
                 sx={{
-                  backgroundColor: "#1b8b60",
+                  backgroundColor: hasAllIngredientsAvailable
+                    ? "rgba(0, 0, 0, 0.12)"
+                    : "#1b8b60",
                   borderRadius: 0,
                   "&:hover": {
-                    backgroundColor: "#15724e",
+                    backgroundColor: hasAllIngredientsAvailable
+                      ? "rgba(0, 0, 0, 0.12)"
+                      : "#15724e",
                   },
                 }}
-                startIcon={<AddShoppingCartIcon />}
               >
                 Add to List
               </Button>
             </Box>
-          </CardContainer>
+          </StyledCard>
         </Grid>
 
-        {/* Nutrition Information */}
-        <Grid item xs={12} md={6}>
-          <CardContainer>
-            <Typography gutterBottom variant="h6" component="div">
-              Nutrition Information
-            </Typography>
-            <List dense>
-              {Object.entries(meal.NutritionalInformation).map(
-                ([key, value]) => (
-                  <ListItem key={key}>{`${key}: ${value}`}</ListItem>
-                )
-              )}
-            </List>
-          </CardContainer>
+        <Grid item xs={12} md={4.5}>
+          <StyledCard>
+            <StyledCardMedia
+              component="img"
+              image={meal.mealImage || "defaultMealImage.jpg"}
+              alt={meal.mealName}
+            />
+          </StyledCard>
         </Grid>
 
-        {/* Preparation Method */}
-        <Grid item xs={12} md={6}>
-          <CardContainer>
-            <Typography gutterBottom variant="h6" component="div">
-              Method
+        <Grid item xs={12} md={4}>
+          <StyledCard>
+            <Typography
+              gutterBottom
+              variant="h6"
+              component="div"
+              sx={{ color: "#1b8b60" }}
+            >
+              Nutritional Facts
             </Typography>
-            <Typography variant="body1" paragraph>
-              {meal.PreparationMethod}
-            </Typography>
-          </CardContainer>
+            <NutritionInfo>
+              <div>
+                <NutritionLabel>Fat:</NutritionLabel>
+                <Typography>
+                  {meal.nutritionalInformation?.Fat || "N/A"}
+                </Typography>
+              </div>
+              <div>
+                <NutritionLabel>Calories:</NutritionLabel>
+                <Typography>
+                  {meal.nutritionalInformation?.Calories || "N/A"}
+                </Typography>
+              </div>
+              <div>
+                <NutritionLabel>Protein:</NutritionLabel>
+                <Typography>
+                  {meal.nutritionalInformation?.Protein || "N/A"}
+                </Typography>
+              </div>
+            </NutritionInfo>
+
+            <Box sx={{ display: "flex", justifyContent: "center", mt: 2 }}>
+              <Button
+                variant="contained"
+                disabled={!hasAllIngredientsAvailable}
+                sx={{
+                  backgroundColor: !hasAllIngredientsAvailable
+                    ? "rgba(0, 0, 0, 0.12)"
+                    : "#1b8b60",
+                  borderRadius: 0,
+                  "&:hover": {
+                    backgroundColor: !hasAllIngredientsAvailable
+                      ? "rgba(0, 0, 0, 0.12)"
+                      : "#15724e",
+                  },
+                }}
+              >
+                Consume
+              </Button>
+            </Box>
+          </StyledCard>
         </Grid>
       </Grid>
     </Container>
   );
 };
+
+// Simulated function; need to replace with actual availability check
+/*
+const ingredientAvailability = (ingredient) => {
+  // Simulated check; replace with actual availability check
+  console.log(ingredient);
+  return Math.random() > 0.3; // 70% chance of ingredient being available
+};
+*/
 
 export default MealDetails;
