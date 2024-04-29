b6ca8cd63f11cc45a5e3798d24b59373331eee18
zayyehtut <zayyehtut.zy@gmail.com>
2024-04-29 13:33:12 +1000

    Update mealService.js for real-time data

--- src/services/mealService.js
+++ src/services/mealService.js
@@ -1,277 +1,204 @@
-// src/services/mealService.js
-let base_url = "https://agile-atoll-76917-ba182676f53b.herokuapp.com/";
+import axios from "axios";
 
-//Mock data for members
-const members = [
-  {
-    id: "6f3cbbd0-8f99-479f-8ad3-e0761db5ec81",
-    name: "Ranko",
-    age: 20,
-    allergies: [],
-    preference: [],
-    height: 170,
-    weight: 60,
-    imageURL: "https://api.dicebear.com/8.x/adventurer/svg?seed=Lilly",
-  },
-  {
-    id: "5ea8e200-e60b-49ff-9223-2858b06bbe43",
-    name: "Poh",
-    age: 20,
-    allergies: [],
-    preference: [],
-    height: 170,
-    weight: 60,
-    imageURL: "https://api.dicebear.com/8.x/adventurer/svg?seed=Lily",
-  },
-  {
-    id: "c03b3300-fe16-4617-8b41-77ba3fd87baa",
-    name: "Sreeram",
-    age: 20,
-    allergies: [],
-    preference: [],
-    height: 170,
-    weight: 60,
-    imageURL: "https://api.dicebear.com/8.x/adventurer/svg?seed=Bandit",
-  },
-  {
-    id: "27eaabf4-e2df-4bd3-b6c3-38b08aa3e796",
-    name: "Christine",
-    age: 20,
-    allergies: [],
-    preference: [],
-    height: 170,
-    weight: 60,
-    imageURL: "https://api.dicebear.com/8.x/adventurer/svg?seed=Callie",
-  },
-  {
-    id: "bb159d3a-f464-44d4-a8c5-9b334e12d8a8",
-    name: "Thomas",
-    age: 20,
-    allergies: ["Duck"],
-    preference: ["Chicken breast", "Peanuts"],
-    height: 170,
-    weight: 60,
-    imageURL: "https://api.dicebear.com/8.x/adventurer/svg?seed=Nala",
-  },
-];
+let base_url = "";
 
-// Mock data for fridges and freezers
-const mockFridges = [
-  {
-    id: "2176cb2a-81f3-445a-9e21-2b35ebaff56c",
-    storageName: "F1",
-    capacity: 1,
-    type: "Fridge",
-    items: [
-      {
-        itemID: 90,
-        foodName: "Applesauce",
-        quantity: 10,
-        expiryDate: "2024-04-25",
-        foodID: {
-          id: 1,
-          per100grams: "100g",
-          foodCategory: "CannedFruit",
-          foodItem: "Applesauce",
-          cals_per100grams: "62 cal",
-          kj_per100grams: "260 kJ",
-        },
-      },
-      {
-        itemID: 101,
-        foodName: "Chicken breast",
-        quantity: 5,
-        expiryDate: "2024-04-30",
-        foodID: {
-          id: 562,
-          per100grams: "100g",
-          foodCategory: "ColdCuts&LunchMeat",
-          foodItem: "Chicken breast",
-          cals_per100grams: "79 cal",
-          kj_per100grams: "332 kJ",
-        },
-      },
-      {
-        itemID: 102,
-        foodName: "Peanuts",
-        quantity: 2,
-        expiryDate: "2024-05-15",
-        foodID: {
-          id: 1547,
-          per100grams: "100g",
-          foodCategory: "Legumes",
-          foodItem: "Peanuts",
-          cals_per100grams: "567 cal",
-          kj_per100grams: "2381 kJ",
-        },
-      },
-      {
-        itemID: 103,
-        foodName: "Bell peppers",
-        quantity: 3,
-        expiryDate: "2024-04-28",
-        foodID: {
-          id: 2149,
-          per100grams: "100g",
-          foodCategory: "Vegetables",
-          foodItem: "Bell peppers",
-          cals_per100grams: "260 cal",
-          kj_per100grams: "1224 kJ",
-        },
-      },
-      {
-        itemID: 104,
-        foodName: "Dried chili peppers",
-        quantity: 1,
-        expiryDate: "2024-06-01",
-        foodID: {
-          id: 2171,
-          per100grams: "100g",
-          foodCategory: "Vegetables",
-          foodItem: "Dried chili peppers",
-          cals_per100grams: "100 cal",
-          kj_per100grams: "985 kJ",
-        },
-      },
-      {
-        itemID: 105,
-        foodName: "Soy sauce",
-        quantity: 1,
-        expiryDate: "2024-07-01",
-        foodID: {
-          id: 2210,
-          per100grams: "100g",
-          foodCategory: "Condiments",
-          foodItem: "Soy sauce",
-          cals_per100grams: "275 cal",
-          kj_per100grams: "627 kJ",
-        },
-      },
-    ],
-  },
-  // ... (other fridges)
-];
+const axiosInstance = axios.create({
+  baseURL: base_url,
+});
 
-const mockFreezers = [
-  {
-    id: "3062e7e8-6a83-4729-aeec-a35576930949",
-    storageName: "Freezer 1",
-    capacity: 10,
-    type: "Freezer",
-    items: [
-      {
-        itemID: 94,
-        foodName: "Applesauce",
-        quantity: 9,
-        expiryDate: "2024-04-23",
-        foodID: {
-          id: 1,
-          per100grams: "100g",
-          foodCategory: "CannedFruit",
-          foodItem: "Applesauce",
-          cals_per100grams: "62 cal",
-          kj_per100grams: "260 kJ",
-        },
-      },
-    ],
-  },
-  // ... (other freezers)
-];
+let cache = {
+  userData: null,
+};
+
+const getUser = async () => {
+  if (cache.userData) {
+    return cache.userData;
+  }
+  try {
+    const response = await axiosInstance.get(
+      `/api/user/${localStorage.getItem("UUID")}`
+    );
+    cache.userData = response.data;
+    return response.data;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
+
+const getMembers = async () => {
+  try {
+    const data = await getUser();
+    return data.members;
+  } catch (error) {
+    console.error("Error fetching members:", error);
+    throw error;
+  }
+};
 
 const getMeals = async () => {
   try {
-    const response = await fetch(base_url + "meal_plans");
-    if (!response.ok) {
-      throw new Error(`HTTP error ${response.status}`);
-    }
-    const data = await response.json();
-    if (!Array.isArray(data)) {
-      throw new Error("Malformed response: expected an array");
-    }
-    return data;
+    const response = await axiosInstance.get("meal_plans");
+    return response.data;
   } catch (error) {
     console.error("Error fetching meals:", error);
-    // Handle error, e.g., display a user-friendly error message or redirect to an error page
-    return [];
+    throw error;
   }
 };
 
-const getMealsFilteredByMemberAllergies = (memberId) => {
-  const member = members.find((m) => m.id === memberId);
-  if (!member) return Promise.resolve(getMeals()); // Return all meals if member not found
+const getMealsFilteredByMemberAllergies = async (memberId) => {
+  try {
+    const members = await getMembers();
+    const member = members.find((m) => m.id === memberId);
+    if (!member) {
+      return await getMeals();
+    }
 
-  return getMeals().then((meals) => {
-    // Filter meals by checking that none of the member's allergies are in the meal's ingredients
-    const memberAllergies = member.allergies.map((allergy) =>
-      allergy.toLowerCase()
+    const memberAllergies = new Set(
+      member.allergies.map((allergy) => allergy.toLowerCase())
     );
-    const filteredMeals = meals.filter(
-      (meal) =>
-        !meal.ingredients.some((ingredient) =>
-          memberAllergies.includes(ingredient.toLowerCase())
-        )
+    return (await getMeals()).filter((meal) => {
+      const ingredients = meal.ingredients.map((ingredient) =>
+        ingredient.toLowerCase()
+      );
+      return !ingredients.some((ingredient) => memberAllergies.has(ingredient));
+    });
+  } catch (error) {
+    console.error("Error in getMealsFilteredByMemberAllergies:", error);
+    throw error;
+  }
+};
+
+const getMealById = async (mealId) => {
+  try {
+    const meals = await getMeals();
+    return meals.find((meal) => meal.id === mealId);
+  } catch (error) {
+    console.error("Error fetching meal by ID:", error);
+    throw error;
+  }
+};
+
+const ingredientAvailability = async (ingredients) => {
+  try {
+    const userData = await getUser();
+
+    const allItems = [
+      ...userData.fridges.flatMap((fridge) => fridge.items),
+      ...userData.freezers.flatMap((freezer) => freezer.items),
+    ];
+
+    return ingredients.every((ingredient) =>
+      allItems.some(
+        (item) => item.foodName.toLowerCase() === ingredient.toLowerCase()
+      )
     );
-
-    return Promise.resolve(filteredMeals);
-  });
+  } catch (error) {
+    console.error("Error checking ingredient availability:", error);
+    throw error;
+  }
 };
 
-const getMealById = (mealId) => {
-  return getMeals().then((meals) => {
-    const meal = meals.find((meal) => meal.id === mealId);
-    return Promise.resolve(meal); // Return the found meal or undefined
-  });
-};
+const updateItemQuantity = async (itemId, quantity, storageId) => {
+  const updatedItem = {
+    itemID: itemId,
+    quantity: quantity,
+    expiryDate: "2024-04-02",
+    id: storageId,
+  };
 
-// Assuming that we have an `ingredientAvailability` function that checks if ingredients are available
-// For the sake of simulation, we'll just assume all ingredients are available if they are not allergens
-// Simulated function; need toreplace with actual availability check
-/*
-const ingredientAvailability = (ingredients) => {
-  // Simulated check; replace with actual availability check
-  return ingredients.length <= 5;
-};
-*/
-const ingredientAvailability = (ingredients) => {
-  // Check if all ingredients are available in the fridges and freezers
-  const availableIngredients = new Set();
-
-  // Check fridges
-  mockFridges.forEach((fridge) => {
-    fridge.items.forEach((item) => {
-      availableIngredients.add(item.foodName.toLowerCase());
-    });
-  });
-
-  // Check freezers
-  mockFreezers.forEach((freezer) => {
-    freezer.items.forEach((item) => {
-      availableIngredients.add(item.foodName.toLowerCase());
-    });
-  });
-
-  // Check if all ingredients are available
-  return ingredients.every((ingredient) =>
-    availableIngredients.has(ingredient.toLowerCase())
+  console.log(
+    "Updating item:",
+    updatedItem.itemID,
+    updatedItem.quantity,
+    updatedItem.id
   );
+
+  try {
+    const response = await axios.post(
+      "https://agile-atoll-76917-ba182676f53b.herokuapp.com/api/updateItem",
+      updatedItem
+    );
+
+    console.log("Item updated successfully:", response.data);
+    return response.data;
+  } catch (error) {
+    console.error("Failed to update item:", error.response.data);
+    throw error;
+  }
 };
-const getMealsFilteredByMember = (memberId) => {
-  const member = members.find((m) => m.id === memberId);
-  if (!member) {
-    return Promise.resolve({
-      preferenceMeals: [],
-      readyToEatMeals: [],
-      ingredientsNeededMeals: [],
-    });
+
+const consumeMeal = async (meal) => {
+  if (!meal || !meal.ingredients) {
+    throw new Error("Invalid meal data");
   }
 
-  const allergies = member.allergies.map((allergy) => allergy.toLowerCase());
-  const preferences = (member.preference || []).map((preference) =>
-    preference.toLowerCase()
-  );
+  try {
+    const userData = await mealService.getUser();
 
-  return getMeals().then((meals) => {
-    // Separate meals into categories
+    const updatePromises = meal.ingredients.map(async (ingredientName) => {
+      let updated = false;
+
+      for (const storageType of [userData.fridges, userData.freezers]) {
+        for (const storage of storageType) {
+          const ingredient = storage.items.find(
+            (item) =>
+              item.foodName.toLowerCase() === ingredientName.toLowerCase()
+          );
+
+          if (ingredient && ingredient.quantity > 0) {
+            const newQuantity = ingredient.quantity - 1;
+            console.log(
+              `Updating ${ingredientName} quantity to ${newQuantity} in storage ID ${storage.id}`
+            );
+            await mealService.updateItemQuantity(
+              ingredient.itemID,
+              newQuantity,
+              storage.id
+            );
+            updated = true;
+            break;
+          }
+        }
+        if (updated) break;
+      }
+
+      if (!updated) {
+        console.error(
+          `Ingredient not found or quantity is zero: ${ingredientName}`
+        );
+      }
+    });
+
+    await Promise.all(updatePromises);
+
+    mealService.clearCache();
+  } catch (error) {
+    console.error("Failed to consume meal:", error);
+    throw error;
+  }
+};
+
+const getMealsFilteredByMember = async (memberId) => {
+  try {
+    const members = await getMembers();
+    const member = members.find((m) => m.id === memberId);
+    if (!member) {
+      return {
+        preferenceMeals: [],
+        readyToEatMeals: [],
+        ingredientsNeededMeals: [],
+      };
+    }
+
+    const allergies = Array.isArray(member.allergies)
+      ? member.allergies.map((allergy) => allergy.toLowerCase())
+      : [];
+    const preferences = Array.isArray(member.preference)
+      ? member.preference.map((preference) => preference.toLowerCase())
+      : [];
+
+    const meals = await getMeals();
     let preferenceMeals = [];
     let readyToEatMeals = [];
     let ingredientsNeededMeals = [];
@@ -300,28 +227,42 @@ const getMealsFilteredByMember = (memberId) => {
       }
     });
 
-    // Use a timestamp or another method for unique keys if needed
     preferenceMeals = preferenceMeals.map((meal) => ({ ...meal }));
     readyToEatMeals = readyToEatMeals.map((meal) => ({ ...meal }));
     ingredientsNeededMeals = ingredientsNeededMeals.map((meal) => ({
       ...meal,
     }));
 
-    return Promise.resolve({
+    return {
       preferenceMeals,
       readyToEatMeals,
       ingredientsNeededMeals,
-    });
-  });
+    };
+  } catch (error) {
+    console.error("Error in getMealsFilteredByMember:", error);
+    return {
+      preferenceMeals: [],
+      readyToEatMeals: [],
+      ingredientsNeededMeals: [],
+    };
+  }
+};
+
+const clearCache = () => {
+  cache.userData = null;
 };
 
 const mealService = {
-  members,
+  getUser,
+  getMembers,
   ingredientAvailability,
   getMeals,
   getMealsFilteredByMemberAllergies,
   getMealsFilteredByMember,
   getMealById,
+  clearCache,
+  updateItemQuantity,
+  consumeMeal,
 };
 
 export default mealService;
