eb519379735ad1587e5c976ea3189fb5b33ad19c
zayyehtut <zayyehtut.zy@gmail.com>
2024-04-29 13:31:32 +1000

    Update MealCard.js

--- src/components/meals/MealCard.js
+++ src/components/meals/MealCard.js
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Card, CardMedia, CardContent, Typography, Grid } from "@mui/material";
 import mealService from "../../services/mealService";
 
 const MealCard = ({ meal, onSelect }) => {
-  const isReadyToEat = mealService.ingredientAvailability(meal.ingredients);
+  const [isReadyToEat, setIsReadyToEat] = useState(false);
+
+  useEffect(() => {
+    mealService
+      .ingredientAvailability(meal.ingredients)
+      .then(setIsReadyToEat)
+      .catch((error) => {
+        console.error("Failed to check ingredient availability", error);
+        setIsReadyToEat(false);
+      });
+  }, [meal.ingredients]);
 
   return (
     <Grid item xs={12} sm={6} md={4} lg={3}>
